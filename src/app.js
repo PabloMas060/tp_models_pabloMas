7aa6a433331130a405021a59d6d53b8730e79525
PabloMas060 <pablo.mas.dev@gmail.com>
2023-09-17 14:10:13 -0300

    add genres routes

--- src/app.js
+++ src/app.js
@@ -24,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', indexRouter);
-app.use('/movies', moviesRouter)
+app.use('/movies', moviesRouter);
+app.use('/genres', genresRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

c01d822ceb65e044f33f3d8125bafbd2b308f63a
PabloMas060 <pablo.mas.dev@gmail.com>
2023-09-18 19:52:35 -0300

    add order genres list

--- src/controllers/genresController.js
+++ src/controllers/genresController.js
@@ -2,7 +2,9 @@ const db = require('../database/models')
 
 module.exports = {
     list : (req,res) => {
-        db.Genre.findAll()
+        db.Genre.findAll({
+            order : [['name', 'ASC']]
+        })
         .then((genres) => {
             return res.render('genresList',{
                 genres
